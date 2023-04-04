57cf892268a45a4da20113874948f6795bc90633
Thermospore <35350819+Thermospore@users.noreply.github.com>
2023-04-03 21:01:25 -0800

    basic decoder working!!!

--- NTSC_decoder.m
+++ NTSC_decoder.m
@@ -27,12 +27,41 @@ pulseTiming = zeros(N,1);
 pulseTiming(v > -.2) = 1;
 pulseTiming = [diff(pulseTiming); 0];
 
-% find start and end of line
-lineStart = min(t(pulseTiming == 1));
-lineEnd = min(t(pulseTiming == -1 & t > lineStart));
+% initialize loop vars
+lineEnd = 0;
+lineNo = 1;
+frame = cell(1000,3);
 
-plot(t,[pulseTiming v], lineStart,0,'x', lineEnd,0,'x')
-xlim([lineStart lineEnd])
-ylim([-0.286 0.936])
+% loop through every line in the whole signal
+while (lineNo < 1000)
+    % find start and end of line
+    lineStart = min(t(pulseTiming == 1 & t > lineEnd));
+    lineEnd = min(t(pulseTiming == -1 & t > lineStart));
+    
+    % break at end of recording
+    if (isempty(lineStart) | isempty(lineEnd))
+        break
+    end
 
-lineV = v(t > lineStart & t < lineEnd);
+%     plot(t,[pulseTiming v], lineStart,0,'x', lineEnd,0,'x')
+%     xlim([lineStart lineEnd])
+%     ylim([-0.286 0.936])
+    
+    % grab line voltage
+    lineV = v(t > lineStart & t < lineEnd)';
+    
+    % skip short lines
+    if (length(lineV) < 800)
+        continue
+    end
+    
+    % trim line voltage
+    lineV = lineV(1:7350);
+    
+    % store line data into frame
+    frame(lineNo,:) = {lineStart lineEnd lineV};
+    lineNo = lineNo + 1;
+end
+
+% display frame
+imagesc(cell2mat(frame(:,3)))
